08452d4ff7fe5381d8ac24c6fb4c6a94e9b37276
madroakos <madroboy@protonmail.com>
2024-09-03 11:28:05 +0200

    Include new button functions

--- js/app.js
+++ js/app.js
@@ -2,9 +2,15 @@ const sections = ['about', 'skills', 'portfolio', 'experience', 'contact'];
 
 sections.forEach(section => {
   const element = document.getElementById(section);
-  const button = document.getElementById(`${section}Button`);
+  const button = document.getElementById(`${section}ButtonText`);
+  const symbol = document.getElementById(`${section}ButtonSymbol`);
 
   button.addEventListener('click', (event) => {
+    event.preventDefault();
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  symbol.addEventListener('click', (event) => {
     event.preventDefault();
     element.scrollIntoView({ behavior: 'smooth' });
   });
